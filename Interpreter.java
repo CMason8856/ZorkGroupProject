54ea31b5aaa50f05e1cfc4be8d833c2ab4ea8773
wolf495 <alfredosoto3598@gmail.com>
2017-04-14 11:23:38 -0400

    Add files via upload

--- Interpreter.java
+++ Interpreter.java
@@ -1,3 +1,10 @@
+/**
+ * Interpreter to relay between the Dungeon and the Player
+ *
+ * @author(Collin Mason)
+ * @version(0.01)
+ */
+
 import java.util.Scanner;
 
 
@@ -9,25 +16,28 @@ public class Interpreter {
     public static String USAGE_MSG =
             "Usage: Interpreter borkFile.bork|saveFile.sav.";
 
+	/**
+	 * @param args, contains the name of the .bork|sav file to the read in
+	 * @var command, holds the value of the player inputed command
+	 * @var commandLine, Scanner object to pass commands into the dungeon
+	 */
     public static void main(String args[]) {
-        System.out.println("Enter a file: ");
-        String file = new Scanner(System.in).nextLine();
-        /**
+
         if (args.length < 1) {
             System.err.println(USAGE_MSG);
             System.exit(1);
         }
-        */
+
         String command;
         Scanner commandLine = new Scanner(System.in);
 
         try {
             state = GameState.instance();
-            if (file.endsWith(".bork")) {
-                state.initialize(new Dungeon(file));
+            if (args[0].endsWith(".bork")) {
+                state.initialize(new Dungeon(args[0]));
                 System.out.println("\nWelcome to " +
                         state.getDungeon().getName() + "!");
-            } else if (file.endsWith(".sav")) {
+            } else if (args[0].endsWith(".sav")) {
                 state.restore(args[0]);
                 System.out.println("\nWelcome back to " +
                         state.getDungeon().getName() + "!");
@@ -56,6 +66,10 @@ public class Interpreter {
         }
     }
 
+	/**
+	 * @param commandLine, contains the command value inputed by the player
+	 * @return commandLine.nextLine(), returns an empty commandLine object
+	 */
     private static String promptUser(Scanner commandLine) {
 
         System.out.print("> ");
